635eb281b87dd3404ce4854eb281ac1f1b14fe97
kevinolson07 <57046983+kevinolson07@users.noreply.github.com>
2022-04-30 15:28:32 -0700

    made some changes to GUI to display the zBOM diffs

--- bom_compare.py
+++ bom_compare.py
@@ -16,10 +16,7 @@ def selectBom(bom1, bom2):
         for row1 in csvreader1:
             new_bom.append(row1)
     return old_bom, new_bom
-# print(header)
-# print(old_bom[0][0])
-# print(new_bom[0][0])
-# print(row)
+
 def descChange(old_bom,new_bom):
     descChanges = []
     print("--------------DESCRIPTION CHANGES-----------------")
@@ -31,8 +28,13 @@ def descChange(old_bom,new_bom):
                 if old_bom[i][1] == new_bom[n][1]:
                     continue
                 else:
-                    
+                    print('in else loop')
                     print("Desciption needs to be revised: ", old_bom[i][0], old_bom[i][1], "to", new_bom[n][0], new_bom[n][1])
+                    descChanges.append('hello')
+                    print(descChanges)
+    return descChanges
+
+            
 def qtyChange(old_bom,new_bom):
     print("--------------QTY CHANGES-----------------")
     #checks for QTY changes
@@ -45,7 +47,7 @@ def qtyChange(old_bom,new_bom):
                     print("QTY changed from: ", old_bom[i][0], old_bom[i][2], "to", new_bom[n][0], new_bom[n][2])
 
 def revChange(old_bom,new_bom):
-    # mylist = []
+    revChanges = []
     print("-------------REV CHANGES------------------")
     #checks for rev changes
     for i in range(len(old_bom)):
@@ -59,10 +61,12 @@ def revChange(old_bom,new_bom):
                     ob2 = old_bom[i][3]
                     nb = new_bom[n][0]
                     nb2 = new_bom[n][3]
-                    print("REV changed from: ", old_bom[i][0], old_bom[i][3], "to", new_bom[n][0], new_bom[n][3]) 
+                    print("REV changed from: ", old_bom[i][0], old_bom[i][3], "to", new_bom[n][0], new_bom[n][3])
+                    revlist = "REV changed from: ", old_bom[i][0], old_bom[i][3], "to", new_bom[n][0], new_bom[n][3]
+                    revChanges.append(revlist)
     #                 res = f"REV changed from:  {ob} {ob2} to {nb} {nb2}" 
     #                 mylist.append(res)
-    # return mylist
+    return revChanges
             
             
             # for x in range(len(old_bom)):
@@ -94,94 +98,3 @@ def addParts(old_bom,new_bom):
             print("Added PART: ", new_bom[i])
             print(i, n)
         counter1 = 0
-
-# if __name__ == "__main__":
-#     old_bom, new_bom = selectBom()
-#     descChange()
-#     qtyChange()
-#     revChange()
-#     removeParts()
-#     addParts()
-
-
-
-# import csv 
-
-# old_bom = []
-# new_bom = []
-# bom1 = input("Enter Old Revison of BOM file name: ")
-# bom2 = input("Enter New Revison of BOM file name: ")
-# with open(bom1, 'r') as file:
-#     csvreader = csv.reader(file)
-#     header = next(csvreader)
-#     for row in csvreader:
-#         old_bom.append(row)
-# with open(bom2, 'r') as file:
-#     csvreader1 = csv.reader(file)
-#     header1 = next(csvreader1)
-#     for row1 in csvreader1:
-#         new_bom.append(row1)
-# # print(header)
-# # print(old_bom[0][0])
-# # print(new_bom[0][0])
-# # print(row)
-
-# print("--------------DESCRIPTION CHANGES-----------------")
-# #checks for description changes
-# for i in range(len(old_bom)):
-#     for n in range(len(new_bom)):
-#         if old_bom[i][0] == new_bom[n][0]:
-#             # print("on both boms: ",old_bom[i][0])
-#             if old_bom[i][1] == new_bom[n][1]:
-#                 continue
-#             else:
-#                 print("Desciption needs to be revised: ", old_bom[i][0], old_bom[i][1], "to", new_bom[n][0], new_bom[n][1])
-# print("--------------QTY CHANGES-----------------")
-# #checks for QTY changes
-# for i in range(len(old_bom)):
-#     for n in range(len(new_bom)):
-#         if old_bom[i][0] == new_bom[n][0]:
-#             if old_bom[i][2] == new_bom[n][2]:
-#                 continue
-#             else:
-#                 print("QTY changed from: ", old_bom[i][0], old_bom[i][2], "to", new_bom[n][0], new_bom[n][2])
-# print("-------------REV CHANGES------------------")
-# #checks for rev changes
-# for i in range(len(old_bom)):
-#     for n in range(len(new_bom)):
-#         if old_bom[i][0] == new_bom[n][0]:
-#             if old_bom[i][3] == new_bom[n][3]:
-#                 continue
-#                     # print("no change to these components: ", old_bom[i])
-#             else:
-#                     print("REV changed from: ", old_bom[i][0], old_bom[i][3], "to", new_bom[n][0], new_bom[n][3]) 
-            
-            
-#             # for x in range(len(old_bom)):
-#             #     print(x)
-#             #     for y in range(len(new_bom)):
-#             #         print(y)
-# print("--------------PARTS REMOVED-----------------")
-
-# #checks for removed parts
-# counter = 0
-# for i in range(len(old_bom)):
-#     for n in range(len(new_bom)):
-#         if old_bom[i][0] == new_bom[n][0]:
-#             counter += 1
-#     if counter == 0:
-#         print("Removed PART: ", old_bom[i])
-#     counter = 0
-
-# print("------------PARTS ADDED-------------------")
-# # checks for added parts
-# counter1 = 0
-# for i in range(len(new_bom)):
-#     for n in range(len(old_bom)):
-#         if new_bom[i][0] == old_bom[n][0]:
-#             counter1 += 1
-#     if counter1 == 0:
-#         print("Added PART: ", new_bom[i])
-#         print(i, n)
-#     counter1 = 0
-
--- bom_compare_gui.py
+++ bom_compare_gui.py
@@ -1,7 +1,5 @@
 import PySimpleGUI as sg
-import os.path
 from bom_compare import *
-import subprocess
 import sys
 
 
@@ -13,18 +11,6 @@ layout = [  [sg.Text('Browse for Bom 1'), sg.FileBrowse()],
 
 # Create the Window
 window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-
-# def runCommand(cmd, timeout=None, window=None):
-#     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#     output = ''
-#     for line in p.stdout:
-#         line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'backslashreplace').rstrip()
-#         output += line
-#         print(line)
-#         window.Refresh() if window else None        # yes, a 1-line if, so shoot me
-#     retval = p.wait(timeout)
-#     return (retval, output)
 
 
 
@@ -34,10 +20,6 @@ window = sg.Window('Window Title', layout)
 
 if __name__ ==   "__main__":    
     while True:
-
-
-
-
         event, values = window.read()
         bom1 = values["Browse"]
         bom2 = values["Browse0"]
@@ -46,14 +28,19 @@ if __name__ ==   "__main__":
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
         elif event == "Ok":
-            # runCommand(cmd=values['_IN_'], window=window)
             old_bom, new_bom = selectBom(bom1, bom2)
-            descChange(old_bom, new_bom)
-            qtyChange(old_bom, new_bom)
-            revChange(old_bom, new_bom)
-            # print("here")
-            # print(output)
-            # for i in output:
-            #     [sg.Text(i)]
-            removeParts(old_bom, new_bom)
-            addParts(old_bom, new_bom)
+            desc = descChange(old_bom, new_bom)
+            qty = qtyChange(old_bom, new_bom)
+            rev = revChange(old_bom, new_bom)
+            rmPart = removeParts(old_bom, new_bom)
+            addPart = addParts(old_bom, new_bom)
+            # runCommand(cmd=values['_IN_'], window=window)
+            sg.theme('DarkAmber')   # Add a touch of color
+            # All the stuff inside your window.
+            print(desc)
+            layout = [  [sg.Text(desc)],
+            [sg.Text(qty)],
+            [sg.Text(rev)],
+            [sg.Text(rmPart)]
+             ]
+            window = sg.Window('Window Title', layout)
